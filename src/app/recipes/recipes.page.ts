72cd473acd01500c6e150fcdf8e95c706740fa36
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-10-25 20:42:36 +0100

    complete recipe

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Recipe } from '../models/recipe';
 import { RecipeService } from '../services/recipe.service';
 
@@ -7,14 +7,35 @@ import { RecipeService } from '../services/recipe.service';
   templateUrl: './recipes.page.html',
   styleUrls: ['./recipes.page.scss'],
 })
-export class RecipesPage implements OnInit {
+export class RecipesPage implements OnInit, OnDestroy {
 
   recipes: Recipe[];
 
   constructor(private recipeService: RecipeService ) { }
 
   ngOnInit() {
+    console.log('Recipes Page on Init');
+  }
+
+  ngOnDestroy(){
+    console.log('Recipes Page on Destroy');
+  }
+
+  ionViewWillEnter(){
     this.recipes=this.recipeService.getAllRecipes();
+    console.log('Recipes Page Will Enter');
+  }
+
+  ionViewDidEnter(){
+    console.log('Recipes Page Did Enter');
+  }
+
+  ionViewWillLeave(){
+    console.log('Recipes Page Will Leave');
+  }
+
+  ionViewDidLeave(){
+    console.log('Recipes Page Did Leave');
   }
 
 }
