e579fb19e22730ae7596a976d8aee6d83b2333b6
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-11-08 20:48:20 +0100

    add camera controle

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -9,11 +9,12 @@ import { Place } from '../place';
 })
 export class DiscoverPage implements OnInit {
   loadedPlaces: Place[];
-
+  listLoadedPlaces: Place[];
   constructor(private placesServices: PlacesService) { }
 
   ngOnInit() {
     this.loadedPlaces = this.placesServices.getPlaces();
+    this.listLoadedPlaces = this.loadedPlaces.slice(1);
   }
 
 }
--- src/app/places/offers/offers.page.ts
+++ src/app/places/offers/offers.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { IonItemSliding } from '@ionic/angular';
 import { PlacesService } from 'src/app/services/places.service';
 import { Place } from '../place';
 
@@ -9,10 +11,17 @@ import { Place } from '../place';
 })
 export class OffersPage implements OnInit {
   offers: Place[];
-  constructor(private placesService: PlacesService) { }
+  constructor(private placesService: PlacesService
+            , private router: Router) { }
 
   ngOnInit() {
     this.offers = this.placesService.getPlaces();
+  }
+
+  onEdit(offerID: string,slidingItem: IonItemSliding){
+    slidingItem.close();
+    this.router.navigate(['/','places','tabs', 'offers','edit',offerID ]);
+    
   }
 
 }
